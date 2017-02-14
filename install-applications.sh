c3a1d9946aa266458047e42783bc6cffa4a1ab74
Elias da Rosa <elias@eliasdarosa.com.br>
2017-02-14 12:10:27 -0200

    Adicionado novos programas

--- install-applications.sh
+++ install-applications.sh
@@ -8,8 +8,13 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
-echo "Install Filezilla"
-sudo apt-get install filezilla
-
 echo "Install Commons Applications "
+sudo apt-get install filezilla
 sudo apt-get install ark
+sudo apt-get install teamviewer
+sudo apt-get install google-chrome-stable
+
+
+wget http://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb
+sudo dpkg -i dbeaver-ce_latest_amd64.deb
+rm -f dbeaver-ce_latest_amd64.deb
